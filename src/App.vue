79e95cd356f8c78b8474ba1ad4585602c4480fe5
leaf <1486280113@qq.com>
2023-02-27 14:01:37 +0800

    finish dev test

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <el-button>Default</el-button>
+      <el-button>Default</el-button>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
