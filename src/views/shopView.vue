b1e792006296a78f07031131881c6f1af3ba2afc
leaf <1486280113@qq.com>
2023-03-01 14:02:21 +0800

    更新了主页面,制作了一个导航栏

---
+++ src/views/shopView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="content">hello!I'm page of shop...</div>
+</template>
+
+<style scoped>
+
+#content {
+    display: flex;
+    margin-top: 100px;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+}
+
+</style>
